db3d4c179682687006ecf75ce8f499e965f6ec7a
Samuel Fleischer <fleischer.samuel@gmail.com>
2016-10-20 16:59:38 -0700

    Adding the second homework...

--- Ecology/Homework/hw_2-oct_19/Problem_2.r
+++ Ecology/Homework/hw_2-oct_19/Problem_2.r
@@ -1,28 +1,37 @@
-# Number of juveniles
-js=c(0.56, 0.64, 0.3, 0.4, 0, 0.38, 0.18, 0.25, 0.44);
-# Number of adults
-as=c(0.53, 0.68, 0.71, 0.38, 0.54, 0.69, 0.66, 0.49, 0.61); 
-# Fecundity
-f=c(3.38, 1.27, 2.77, 2.17, 0.05, 4.0, 2.37, 0.5, 1.6);
+R_A = c(1.1, 0.9, 1.2, 0.8)
+R_B = c(2.0, 0.4, 3.0, 0.5)
 
-# Fecundity is divided by two since only females produce offspring (we are assuming a 1-1 sex ratio)
-R = as + (f/2)*js
-r = mean(log(R))
+r_A = mean(log(R_A))
+r_B = mean(log(R_B))
+
+sig_A = mean((log(R_A) - r_A)^2)
+sig_B = mean((log(R_B) - r_B)^2)
 
 # Set number of time steps
-Tmax = 500
+Tmax = 25
 # Set number of replications
 reps = 1000
 # Set initial condition
-N0 = 100
+N0 = 500
 # Initialize a matrix of instances (columns)
-N = matrix(N0,nrow=Tmax, ncol=reps)
+N_A = matrix(N0, nrow=Tmax, ncol=reps)
+N_B = matrix(N0, nrow=Tmax, ncol=reps)
 
 # Run all simulations at once
 for(t in 2:Tmax) {
-  N[t,]=N[t-1,]*sample(x=R, size=reps, replace = TRUE)
+  N_A[t,]=N_A[t-1,]*sample(x=R_A, size=reps, replace = TRUE)
+  N_B[t,]=N_B[t-1,]*sample(x=R_B, size=reps, replace = TRUE)
 }
-# Plot the results and the mean trend line
-matplot(log(N),type="l")
-abline(a = log(100), b = mean(log(R)))
 
+attach(mtcars)
+par(mfrow=c(1,2))
+
+# Plot the results and the mean trend line
+matplot(log(N_A),type="l")
+abline(a = log(N0), b = mean(log(R_A)))
+
+matplot(log(N_B),type="l")
+abline(a = log(N0), b = mean(log(R_B)))
+
+A_ext = sum(N_A[Tmax,] > 2)/reps
+B_ext = sum(N_B[Tmax,] > 2)/reps

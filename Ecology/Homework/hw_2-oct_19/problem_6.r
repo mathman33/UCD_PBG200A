d7c02b6a6415a7447692b0ba469445c0e2460b57
Samuel Fleischer <fleischer.samuel@gmail.com>
2016-11-02 11:59:54 -0700

    small changes

--- Ecology/Homework/hw_2-oct_19/problem_6.r
+++ Ecology/Homework/hw_2-oct_19/problem_6.r
@@ -1,8 +1,13 @@
 #getting the San Jose data set (data thiefed)
 load("../../R_files_and_data/McLaughlin.Rdata")
 
+r0 = function(W){model$coeff[1]+model$coeff[3]*W^(-2)}
+
 pre1971indices=which(SanJose$year<1971)
 pos1970indices=which(SanJose$year>1970)
 
-r0 = function(W){model$coeff[1]+model$coeff[3]*W^(-2)}
+one = SJtransformed[pre1971indices]
+two = SJtransformed[pos1970indices]
 
+mean(r0(one))
+mean(r0(two))
